abd266dbe3cab28f563852e1ee1dfe3848e54f10
Ben Daniels <benbanj@hotmail.com>
2020-01-26 16:45:24 +0000

    Start #move method in Game class

--- game.rb
+++ game.rb
@@ -1,3 +1,22 @@
+require './board'
+require './player'
+
 class Game
+  attr_accessor :board
+  
+  def initialize
+    @board = Board.new
+  end
+
+  def move(start, finish)
+    # 2 sets of coordinates 
+    # check to see what piece is in that start position
+    # eg "a1 a3"
+    let, num = start.chars
+    board.return_board[num.to_i - 1][let.ord - 97]
+    # returns ^ start piece 
+    # what moves it has available and verify the move is valid
+    # move piece
+  end
   
 end
