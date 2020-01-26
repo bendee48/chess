53359416328f56e2f4e857929739df91965b263d
Ben Daniels <benbanj@hotmail.com>
2020-01-26 15:57:28 +0000

    Updating #display_board presentation.

--- board.rb
+++ board.rb
@@ -6,10 +6,11 @@ class Board
 
   def initialize
     create_board
+    populate_board
   end
 
   def create_board
-    (1..8).each { |num| instance_variable_set("@row_#{num}", Array.new(8)) }
+    (1..8).each { |num| instance_variable_set("@row_#{num}", Array.new(8, "-")) }
   end
 
   def return_board
@@ -17,21 +18,24 @@ class Board
   end
 
   def display_board
+    num = 8
+    puts "\n    a b c d e f g h"
+    puts
     return_board.reverse_each do |row|
-      row = row.map { |piece| piece.unicode if piece.is_a?(ChessPiece) }
-      puts " %s %s %s %s %s %s %s %s" % row
+      row = row.map { |piece| piece.is_a?(ChessPiece) ? piece.unicode : piece }
+      puts " #{num}  %s %s %s %s %s %s %s %s  #{num}" % row
+      num -= 1  
     end
-    nil
+    puts
+    puts "    a b c d e f g h\n"
   end
+
+  private
 
   def populate_board
     populate_royalty
     populate_pawns
   end
-
-  private
-
-  
 
   def generate_piece(num)
     (1..num).map { yield }
