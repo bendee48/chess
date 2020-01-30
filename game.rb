b4652bd1446fbbb9e9a46388116f016fff30d641
Ben Daniels <benbanj@hotmail.com>
2020-01-30 19:42:06 +0000

    Add rook helper methods

--- game.rb
+++ game.rb
@@ -14,84 +14,48 @@ class Game
 
     case start_piece
     when "pawn"
-      if valid_pawn_move?(start, finish, player)
-        start_let, start_num = start.scan(/[a-z]|\d+/)
-        finish_let, finish_num = finish.scan(/[a-z]|\d+/)
-        board.return_board[finish_num.to_i - 1][finish_let.ord - 97] = return_piece(start)
-        board.return_board[start_num.to_i - 1][start_let.ord - 97] = "-"
+      possible_moves = possible_pawn_moves(start, player)
+
+      if valid_move?(possible_moves, finish)
+        move_piece(start, finish)
       else
         puts "Sorry, invalid move."
       end
     when "rook"
-      valid_moves = []
+      possible_moves = possible_rook_moves(start, player)
 
-      current_square = start
-      loop do #up
-        current_square = current_square.next
-        (valid_moves << current_square; next) if return_piece(current_square) == "-"
-        (valid_moves << current_square; break) if return_piece(current_square).color != player.color &&
-                                                  return_piece(current_square).name != "king"
-        break if return_piece(current_square).color == player.color
+      if valid_move?(possible_moves, finish)
+        move_piece(start, finish)    
+      else
+        puts "Sorry, invalid move."    
       end
-      #down
-      current_square = start
-      loop do
-        current_let, current_num = current_square.scan(/[a-z]|\d+/)
-        current_square = "#{current_let}#{current_num.to_i - 1}"
-        break if return_piece(current_square) == "error"
-        (valid_moves << current_square; next) if return_piece(current_square) == "-"
-        (valid_moves << current_square; break) if return_piece(current_square).color != player.color &&
-                                                  return_piece(current_square).name != "king"
-        break if return_piece(current_square).color == player.color
-      end
-
-      #right
-      current_square = start
-      loop do
-        current_let, current_num = current_square.scan(/[a-z]|\d+/)
-        current_square = "#{current_let.next}#{current_num}"
-        break if return_piece(current_square) == "error"
-        (valid_moves << current_square; next) if return_piece(current_square) == "-"
-        (valid_moves << current_square; break) if return_piece(current_square).color != player.color &&
-                                                  return_piece(current_square).name != "king"
-        break if return_piece(current_square).color == player.color
-      end
-
-      #left
-      current_square = start
-      loop do
-        current_let, current_num = current_square.scan(/[a-z]|\d+/)
-        current_square = "#{(current_let.ord - 1).chr}#{current_num}"
-        break if return_piece(current_square) == "error"
-        (valid_moves << current_square; next) if return_piece(current_square) == "-"
-        (valid_moves << current_square; break) if return_piece(current_square).color != player.color &&
-                                                  return_piece(current_square).name != "king"
-        break if return_piece(current_square).color == player.color
-      end
-
-      if valid_moves.include?(finish)
-        start_let, start_num = start.scan(/[a-z]|\d+/)
-        finish_let, finish_num = finish.scan(/[a-z]|\d+/)
-        board.return_board[finish_num.to_i - 1][finish_let.ord - 97] = return_piece(start)
-        board.return_board[start_num.to_i - 1][start_let.ord - 97] = "-"
-      end
-
-
     end
 
   end
 
   private
 
-  def valid_pawn_move?(start, finish, player)
-    valid_moves = possible_pawn_moves(start, player)
-    valid_moves.include?(finish)    
+  def return_piece(coordinates)
+    let, num = coordinates.scan(/[a-z]|\d+/)
+    return "error" unless ("a".."h") === let && ("1".."8") === num
+    board.return_board[num.to_i - 1][let.ord - 97]
+  end 
+
+  def move_piece(start, finish)
+    start_let, start_num = start.scan(/[a-z]|\d+/)
+    finish_let, finish_num = finish.scan(/[a-z]|\d+/)
+    board.return_board[finish_num.to_i - 1][finish_let.ord - 97] = return_piece(start)
+    board.return_board[start_num.to_i - 1][start_let.ord - 97] = "-"
+  end
+
+  def valid_move?(moves, finish)
+    moves.include?(finish)
   end
 
   def possible_pawn_moves(start, player)
     let, num = start.scan(/[a-z]|\d+/)
     valid_moves = []
-
+    
     one_up = start.next
     valid_moves << one_up if return_piece(one_up) == "-"
 
@@ -111,10 +75,58 @@ class Game
     valid_moves
   end
 
-  def return_piece(coordinates)
-    let, num = coordinates.scan(/[a-z]|\d+/)
-    return "error" unless ("a".."h") === let && ("1".."8") === num
-    board.return_board[num.to_i - 1][let.ord - 97]
-  end  
+  def possible_rook_moves(start, player)
+    valid_moves = []
+
+    current_square = start
+    loop do #up
+      current_let, current_num = current_square.scan(/[a-z]|\d+/)
+      current_square = "#{current_let}#{current_num.to_i + 1}"
+      break if return_piece(current_square) == "error"
+      (valid_moves << current_square; next) if return_piece(current_square) == "-"
+      (valid_moves << current_square; break) if return_piece(current_square).color != player.color &&
+                                                return_piece(current_square).name != "king"
+      break if return_piece(current_square).color == player.color
+    end
+    #down
+    current_square = start
+    loop do
+      current_let, current_num = current_square.scan(/[a-z]|\d+/)
+      current_square = "#{current_let}#{current_num.to_i - 1}"
+      break if return_piece(current_square) == "error"
+      (valid_moves << current_square; next) if return_piece(current_square) == "-"
+      (valid_moves << current_square; break) if return_piece(current_square).color != player.color &&
+                                                return_piece(current_square).name != "king"
+      break if return_piece(current_square).color == player.color
+    end
+
+    #right
+    current_square = start
+    loop do
+      current_let, current_num = current_square.scan(/[a-z]|\d+/)
+      current_square = "#{(current_let.ord + 1).chr}#{current_num}"
+      break if return_piece(current_square) == "error"
+      (valid_moves << current_square; next) if return_piece(current_square) == "-"
+      (valid_moves << current_square; break) if return_piece(current_square).color != player.color &&
+                                                return_piece(current_square).name != "king"
+      break if return_piece(current_square).color == player.color
+    end
+
+    #left
+    current_square = start
+    loop do
+      current_let, current_num = current_square.scan(/[a-z]|\d+/)
+      current_square = "#{(current_let.ord - 1).chr}#{current_num}"
+      break if return_piece(current_square) == "error"
+      (valid_moves << current_square; next) if return_piece(current_square) == "-"
+      (valid_moves << current_square; break) if return_piece(current_square).color != player.color &&
+                                                return_piece(current_square).name != "king"
+      break if return_piece(current_square).color == player.color
+    end
+
+    valid_moves
+  end
+
+   
   
 end
