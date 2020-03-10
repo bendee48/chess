f7f70c0e95cdded33395556825d2566376dd40f8
Ben Daniels <benbanj@hotmail.com>
2020-03-10 13:46:47 +0000

    Memoize Moves

--- moves.rb
+++ moves.rb
@@ -16,15 +16,17 @@ class Moves
   end
 
   def self.diagonal
-    { up_right: [1, 1], up_left: [-1, 1], down_left: [-1, -1], down_right: [1, -1] }
+    @diagonal ||= { 
+      up_right: [1, 1], up_left: [-1, 1], down_left: [-1, -1], down_right: [1, -1] 
+    }
   end
 
   def self.horizontal_vertical
-    { up: [0, 1], down: [0, -1], left: [-1, 0], right: [1, 0] }
+    @horiz_vert ||= { up: [0, 1], down: [0, -1], left: [-1, 0], right: [1, 0] }
   end
 
   def self.knight
-    {
+    @knight ||= {
       up_right1: [2, 1], up_right2: [1, 2], down_right1: [-1, 2], down_right2: [-2, 1],
       left_down1: [-2, -1], left_down2: [-1, -2], up_left1: [1, -2], up_left2: [2, -1]
     }
